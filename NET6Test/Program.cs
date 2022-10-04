964f31a4f79a6be354547d2d70dd761e0e9076a4
AzureADBłażejSzajrych <bs@macrix.pl>
2022-10-04 09:55:33 +0200

    Add project files.

---
+++ NET6Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using ClassLibrary1;
+using System;
+using System.Threading.Tasks;
+
+namespace NET5Test
+{
+  internal class Program
+  {
+    static async Task Main(string[] args)
+    {
+      var c = new Class1();
+      Console.WriteLine($"Hello World! from NET 6.0 + {c.GetType().FullName}");
+
+      while (true)
+      {
+        await Task.Delay(100);
+
+        if (Console.KeyAvailable)
+        {
+          break;
+        }
+      }
+
+      Console.WriteLine("Press enter to exit...");
+      Console.ReadLine();
+    }
+  }
+}
